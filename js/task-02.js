0a3eacc2d55975e704d10b95a5662a40755ab786
Paulina Mlynarska <paulinamlynarska@outlook.com>
2023-11-13 20:54:37 +0000

    Changes 2

--- js/task-02.js
+++ js/task-02.js
@@ -7,11 +7,11 @@ const ingredients = [
   "Condiments",
 ];
 
-const ingredientsList = document.getElementById("ingredients");
+const array = document.getElementById("ingredients");
 
-ingredients.forEach((ingredient) => {
-  const listItem = document.createElement("li");
-  listItem.textContent = ingredient;
-  listItem.classList.add("item");
-  ingredientsList.appendChild(listItem);
+ingredients.map((ingredient) => {
+  let li = document.createElement("li");
+  li.textContent = ingredient;
+  li.setAttribute("class", "item");
+  array.appendChild(li);
 });
